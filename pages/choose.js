c14eed9b2722697d8f6feb412000f442a4e6afa3
Adam-pw <adampithewan@gmail.com>
2022-09-18 15:25:30 +0530

    Deploy build

--- pages/choose.js
+++ pages/choose.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import Navbarhome from '../Components/Navbar/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Carouselhome from '../Components/Carousel/Carousel';
 import React from 'react';
+import {MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
+import Loginhome from '../Components/Login/Login';
+import { GetInTouch } from '../Components/Contact/Contact';
 import { ButtonMenu } from '../Components/Buttons/Button';
 import Footerhome from '../Components/Footer/Footer';
 
