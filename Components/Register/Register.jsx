fb5b69d6ec66fac858174d5f25baaf300dfba081
Nikhil <rathaurnikhil14@gmail.com>
2022-09-18 11:11:58 +0530

    register changes

--- Components/Register/Register.jsx
+++ Components/Register/Register.jsx
@@ -12,15 +12,18 @@ function Register() {
   return (
     <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
 
+      <MDBInput wrapperClass='mb-4' label='First Name' id='form2' type='test'/>
+      <MDBInput wrapperClass='mb-4' label='Middle Name' id='form2' type='test'/>
+      <MDBInput wrapperClass='mb-4' label='Last Name' id='form2' type='test'/>
+      <MDBInput wrapperClass='mb-4' label='Adharcard No' id='form1' type='test'/>
+      <MDBInput wrapperClass='mb-4' label='Phone no.' id='form1' type='test'/>
       <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='text'/>
       <MDBInput wrapperClass='mb-4' label='Passward' id='form1' type='password'/>
       <MDBInput wrapperClass='mb-4' label='Confirm Password' id='form1' type='password'/>
-      <MDBInput wrapperClass='mb-4' label='Adharcard No' id='form1' type='test'/>
       <MDBInput wrapperClass='mb-4' label=' Address' id='form1' type='test'/>
+      <MDBInput wrapperClass='mb-4' label=' Country' id='form1' type='test'/>
       <MDBInput wrapperClass='mb-4' label='City' id='form1' type='test'/>
       <MDBInput wrapperClass='mb-4' label='State' id='form1' type='test'/>
-      <MDBInput wrapperClass='mb-4' label='' id='form1' type='test'/>
-      <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='test'/>
 
       <div className="d-flex justify-content-between mx-3 mb-4">
         <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
